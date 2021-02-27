70f4ad166878a236e615adf12209004ac7da25f8
Taz <h.zzzaman@gmail>
2021-02-27 15:56:36 +0600

    dont pull this

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyB4IiDuQ9GbgEf5W2FTtY5NzjYLhwDTcec")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
